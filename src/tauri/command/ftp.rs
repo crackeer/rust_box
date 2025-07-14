2ff1db9f175be8f964059704fb5f4fe2af971335
liuhu016 <liuhu016@realsee.com>
2025-07-15 07:45:58 +0800

    feat:ftp

--- src/tauri/command/ftp.rs
+++ src/tauri/command/ftp.rs
@@ -1,17 +1,17 @@
+use std::io::Write;
+use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
 use suppaftp::FtpStream;
 use tokio::io::AsyncReadExt;
 use uuid::Uuid;
-use std::net::{ SocketAddr};
-use std::time::Duration;
-use std::io::Write;
 lazy_static::lazy_static! {
-    static ref FTP_CLIENTS: Arc<Mutex<std::collections::HashMap<String, FtpStream>>> = 
+    static ref FTP_CLIENTS: Arc<Mutex<std::collections::HashMap<String, FtpStream>>> =
         Arc::new(Mutex::new(std::collections::HashMap::new()));
 }
 
 #[tauri::command]
-pub  async  fn connect_ftp(
+pub async fn connect_ftp(
     host: &str,
     port: u16,
     username: &str,
@@ -21,7 +21,7 @@ pub  async  fn connect_ftp(
     let addr: SocketAddr = url.parse().expect("invalid hostname");
     let mut ftp = FtpStream::connect_timeout(addr, Duration::from_secs(10))
         .map_err(|e| format!("连接失败: {}", e))?;
-    
+
     ftp.login(username, password)
         .map_err(|e| format!("认证失败: {}", e))?;
 
@@ -35,6 +35,30 @@ pub  async  fn connect_ftp(
 
 #[tauri::command]
 pub async fn disconnect_ftp(key: &str) -> Result<(), String> {
+    let mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
+       let ftp = client
+        .get_mut(key)
+        .ok_or_else(|| "指定的FTP连接不存在".to_string())?;
+    ftp.quit().await.map_err(|e| format!("断开连接失败: {}", e))?;
+    client.remove(key);
+    Ok(())
+}
+
+#[tauri::command]
+pub async fn ftp_list(key: &str, path: &str) -> Result<Vec<String>, String> {
+    let mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
+    let ftp = client
+        .get_mut(key)
+        .ok_or_else(|| "指定的FTP连接不存在".to_string())?;
+
+    let files = ftp
+        .list(Some(&format!("{}", path)))
+        .map_err(|e| format!("获取文件列表失败: {}", e))?;
+    Ok(files)
+}
+
+#[tauri::command]
+pub async fn ftp_upload_file(key: &str, path: &str, local_file: &str) -> Result<(), String> {
     FTP_CLIENTS
         .lock()
         .map_err(|_| "锁获取失败".to_string())?
@@ -47,46 +71,78 @@ pub async fn disconnect_ftp(key: &str) -> Result<(), String> {
 
 #[tauri::command]
 pub async fn ftp_list(key: &str, path: &str) -> Result<Vec<String>, String> {
-    let  mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
-    let ftp =  client.get_mut(key).ok_or_else(|| "指定的FTP连接不存在".to_string())?;
+    let mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
+    let ftp = client
+        .get_mut(key)
+        .ok_or_else(|| "指定的FTP连接不存在".to_string())?;
 
-    let files = ftp.list(Some(&format!("{}", path))).map_err(|e| format!("获取文件列表失败: {}", e))?;
+    let files = ftp
+        .list(Some(&format!("{}", path)))
+        .map_err(|e| format!("获取文件列表失败: {}", e))?;
     Ok(files)
 }
 
 #[tauri::command]
 pub async fn ftp_upload_file(key: &str, path: &str, local_file: &str) -> Result<(), String> {
-    let  mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
-    let ftp =  client.get_mut(key).ok_or_else(|| "指定的FTP连接不存在".to_string())?;
+    let mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
+    let ftp = client
+        .get_mut(key)
+        .ok_or_else(|| "指定的FTP连接不存在".to_string())?;
     let mut reader = std::fs::File::open(local_file).map_err(|e| format!("打开文件失败: {}", e))?;
-    ftp.put_file(&path, &mut reader).map_err(|e| format!("上传文件失败: {}", e))?;
+    ftp.put_file(&path, &mut reader)
+        .map_err(|e| format!("上传文件失败: {}", e))?;
     Ok(())
 }
 
 #[tauri::command]
 pub async fn ftp_download_file(key: &str, path: &str, local_file: &str) -> Result<(), String> {
-    let  mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
-    let ftp =  client.get_mut(key).ok_or_else(|| "指定的FTP连接不存在".to_string())?;
-    let mut buf = ftp.retr_as_buffer(&path).map_err(|e| format!("下载文件失败: {}", e))?;
+    let mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
+    let ftp = client
+        .get_mut(key)
+        .ok_or_else(|| "指定的FTP连接不存在".to_string())?;
+    let mut buf = ftp
+        .retr_as_buffer(&path)
+        .map_err(|e| format!("下载文件失败: {}", e))?;
     let mut data_buf = Vec::new();
 
     if std::path::Path::new(&local_file).exists() {
         let mut f = std::fs::File::open(local_file).map_err(|e| format!("打开文件失败: {}", e))?;
-        buf.read_buf(&mut data_buf);
-        f.write_all(&data_buf).map_err(|e| format!("写入文件失败: {}", e))?;
-
+        if let Err(e) = buf.read_buf(&mut data_buf).await {
+            return Err(format!("读取文件失败: {}", e));
+        }
+        f.write_all(&data_buf)
+            .map_err(|e| format!("写入文件失败: {}", e))?;
     } else {
-        let mut f = std::fs::File::create(local_file).map_err(|e| format!("创建文件失败: {}", e))?;
-        buf.read_buf(&mut data_buf);
-        f.write_all(&data_buf).map_err(|e| format!("写入文件失败: {}", e))?;
+        let mut f =
+            std::fs::File::create(local_file).map_err(|e| format!("创建文件失败: {}", e))?;
+        if let Err(e) = buf.read_buf(&mut data_buf).await {
+            return Err(format!("读取文件失败: {}", e));
+        }
+
+        f.write_all(&data_buf)
+            .map_err(|e| format!("写入文件失败: {}", e))?;
     }
     Ok(())
 }
 
 #[tauri::command]
 pub async fn ftp_delete_file(key: &str, path: &str) -> Result<(), String> {
-    let  mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
-    let ftp =  client.get_mut(key).ok_or_else(|| "指定的FTP连接不存在".to_string())?;
-    ftp.rm(&format!("{}", path)).map_err(|e| format!("删除文件失败: {}", e))?;
+    let mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
+    let ftp = client
+        .get_mut(key)
+        .ok_or_else(|| "指定的FTP连接不存在".to_string())?;
+    ftp.rm(&format!("{}", path))
+        .map_err(|e| format!("删除文件失败: {}", e))?;
+    Ok(())
+}
+
+#[tauri::command]
+pub async fn ftp_delete_dir(key: &str, path: &str) -> Result<(), String> {
+    let mut client = FTP_CLIENTS.lock().map_err(|_| "锁获取失败".to_string())?;
+    let ftp = client
+        .get_mut(key)
+        .ok_or_else(|| "指定的FTP连接不存在".to_string())?;
+    ftp.rmdir(&format!("{}", path))
+        .map_err(|e| format!("删除目录失败: {}", e))?;
     Ok(())
 }
