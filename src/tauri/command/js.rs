e1ebf22451eb05789704e1dfdf0d70e074598066
simple <seekh@outlook.com>
2025-08-18 07:58:10 +0800

    feat:run quickjs code

--- src/tauri/command/js.rs
+++ src/tauri/command/js.rs
@@ -2,8 +2,10 @@ use super::define::{failure_response, success_response, InvokeResponse, Message}
 use serde_json::json;
 use std::{ process::Command};
 use tauri;
-//use quickjs_runtime::builder::QuickJsRuntimeBuilder;
-//use quickjs_runtime::jsutils::Script;
+use quickjs_runtime::builder::QuickJsRuntimeBuilder;
+use quickjs_runtime::jsutils::Script;
+use quickjs_runtime::values::JsValueFacade;
+
 #[tauri::command]
 pub fn run_js_code(node_path: String, code: String) -> InvokeResponse {
     match Command::new(&node_path).arg("-e").arg(code).output() {
@@ -35,3 +37,9 @@ pub async fn run_js_script(script_code: String) -> Result<String, String> {
      */
 
 
+#[tauri::command]
+pub async fn run_quick_js_code(script_code: String) -> Result<String, String> {
+    let context = QuickJsRuntimeBuilder::new().build();
+    let result = context.eval_sync(None, Script::new("file://main.js", &script_code)).map_err(|e| e.to_string())?;
+    Ok( result.stringify())
+}
